5911aeabba24c226f1bac75f85d6e698df832bef
external-imageupta <smonichandragupta@deloitte.com>
2022-04-01 10:27:43 +0000

    test

--- test/test-server.js
+++ test/test-server.js
@@ -26,7 +26,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Moni Chandra_Chasers application</h1>"));
         return done();
       });
 
